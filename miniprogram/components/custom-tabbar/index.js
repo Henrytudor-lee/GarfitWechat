6cfe059c357c69477c5218a11d0d07e9645f7104
Zxhy <tlee4014@gmail.com>
2026-05-12 22:49:09 +0800

    replace tabbar icons with new assets, remove unused images and svg files

--- miniprogram/components/custom-tabbar/index.js
+++ miniprogram/components/custom-tabbar/index.js
@@ -9,10 +9,10 @@ Component({
 
   data: {
     tabs: [
-      { id: 'training', label: 'TRAINING', url: '/pages/index/index', icon: '/images/icons/dumbbell.svg', iconActive: '/images/icons/dumbbell-active.svg' },
-      { id: 'library',  label: 'LIBRARY',  url: '/pages/library/index', icon: '/images/icons/library.svg', iconActive: '/images/icons/library-active.svg' },
-      { id: 'stats',    label: 'STATS',    url: '/pages/stats/index', icon: '/images/icons/bar-chart.svg', iconActive: '/images/icons/bar-chart-active.svg' },
-      { id: 'profile',  label: 'PROFILE',  url: '/pages/profile/index', icon: '/images/icons/user.svg', iconActive: '/images/icons/user-active.svg' },
+      { id: 'training', label: 'TRAINING', url: '/pages/index/index', icon: '/images/icons/dumbbell.png', iconActive: '/images/icons/dumbbell-active.png' },
+      { id: 'library',  label: 'LIBRARY',  url: '/pages/library/index', icon: '/images/icons/library.png', iconActive: '/images/icons/library-active.png' },
+      { id: 'stats',    label: 'STATS',    url: '/pages/stats/index', icon: '/images/icons/bar-chart.png', iconActive: '/images/icons/bar-chart-active.png' },
+      { id: 'profile',  label: 'PROFILE',  url: '/pages/profile/index', icon: '/images/icons/user.png', iconActive: '/images/icons/user-active.png' },
     ],
   },
 
